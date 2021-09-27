a70dc4bcbdd3bae815cc6876edb173127fb4b13a
Douglas Higashi <higoshi.douglas@gmail.com>
2021-09-27 19:11:23 -0300

    Inicializando repositório

---
+++ pilha.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void inicPilha(struct pilha *pPilha);
+void empilha(struct pilha *pPilha, int num);
+void desempilha(struct pilha *pPilha);
+void exibirPilha(struct pilha *pPilha);
+
+struct pilha{
+	int topo;
+	int nums[];
+};
+
+struct pilha minhaPilha;
+struct pilha *pp = &minhaPilha;
+
+main(){
+	inicPilha(pp);
+	empilha(pp, 5);
+	empilha(pp, 1);
+	empilha(pp, 2);
+	empilha(pp, 10);//10 é o topo da pilha
+	exibirPilha(pp);
+	printf("\n");
+	
+	desempilha(pp);//retiro o 10 do topo. Agora o 2 e o topo
+	exibirPilha(pp);
+	printf("\n");
+	
+	empilha(pp, 99);//coloco 99 no topo da pilha
+	exibirPilha(pp);
+}
+
+void inicPilha(struct pilha *pPilha){
+	pPilha->topo = -1;
+}
+
+void empilha(struct pilha *pPilha, int num){
+	pPilha->topo++;
+	pPilha->nums[pPilha->topo] = num;
+}
+
+void desempilha(struct pilha *pPilha){
+	int aux = pPilha->nums[pPilha->topo];
+	pPilha->topo--;
+	printf("Numero %d desempilhado\n", aux);
+}
+
+void exibirPilha(struct pilha *pPilha){
+	for(int i = pPilha->topo; i > -1 ; i--){
+		if(i == pPilha->topo){
+			printf("%d - Topo\n", pPilha->nums[pPilha->topo]);
+		}else{
+			printf("%d\n", pPilha->nums[i]);
+		}
+	}
+	
+}
+
